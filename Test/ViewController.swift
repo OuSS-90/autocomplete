2ccedad1928f68dc97ed6343e62ab6dd03187975
OuSS <ouss.cs@gmail.com>
2018-01-01 16:13:41 +0000

    ...

--- Test/ViewController.swift
+++ Test/ViewController.swift
@@ -7,19 +7,41 @@
 //
 
 import UIKit
+import LUAutocompleteView
 
-class ViewController: UIViewController {
-
+class ViewController : UIViewController,LUAutocompleteViewDelegate,LUAutocompleteViewDataSource {
+    
+    // MARK: - Properties
+    
+    @IBOutlet weak var textField: UITextField!
+    private let autocompleteView = LUAutocompleteView()
+    
+    private let elements = (1...4000).map { "\($0)" }
+    
+    // MARK: - ViewController
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        view.addSubview(autocompleteView)
+        
+        autocompleteView.textField = textField
+        autocompleteView.dataSource = self
+        autocompleteView.delegate = self
+        
+        // Customisation
+        
+        autocompleteView.rowHeight = 40
+        //autocompleteView.autocompleteCell = CustomAutocompleteTableViewCell.self // Uncomment this line in order to use customised autocomplete cell
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func autocompleteView(_ autocompleteView: LUAutocompleteView, elementsFor text: String, completion: @escaping ([String]) -> Void) {
+        if text.count < 3 { return }
+        let elementsThatMatchInput = elements.filter { $0.lowercased().contains(text.lowercased()) }
+        completion(elementsThatMatchInput)
     }
-
-
+    
+    func autocompleteView(_ autocompleteView: LUAutocompleteView, didSelect text: String) {
+        print(text + " was selected from autocomplete view")
+    }
 }
-
